4d391a53cb187417fb03f5f3d517136747f08882
User <user@iMacUser.local>
2026-06-18 00:35:17 -0700

    ครั้งที่ 3

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express"); //นำเข้า express เพื่อสร้างเว็บ web server
 const path = require("path");
 const app = express();
+
 app.use(express.json());
+app.use(express.static("public"));
+
 app.get("/students", (req, res) =>{
     res.json([
         {id: 1, name: "Golf"}
     ])
-
 })
-app.listen(3000,() =>{
+app.listen(3000, () =>{
     console.log("Server started");
 })
 
